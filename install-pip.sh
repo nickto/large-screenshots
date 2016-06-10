61a3d0aa77b25155d9e08680113f3caa3ba484e7
Nick To <weederdam@gmail.com>
2016-06-10 15:42:34 +0200

    Add pip, selenium installation scripts

---
+++ install-pip.sh
@@ -0,0 +1,2 @@
+# install pip for python 3
+sudo apt-get install python3-pip
---
+++ install-selenium.sh
@@ -0,0 +1,2 @@
+# install selenium (you need pip3 to run this command, if you do not have it installed, you might want to run insall-pip.sh first)
+sudo pip3 install -U selenium
