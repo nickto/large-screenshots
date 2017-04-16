4583fec49b188631753c21abfedcb2f3255a614f
Nick To <nickto0x2c@gmail.com>
2017-04-16 14:47:35 +0200

    Add comments about running Firefox

--- map-extractor.py
+++ map-extractor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 import os
 from time import sleep
 from selenium import webdriver
@@ -9,10 +8,20 @@ dirName = "out"
 if not os.path.exists(dirName):
     os.makedirs(dirName)
 
-url = "http://balticmaps.eu/?lang=lv&draw_hash=dteamu&centerx=576912.270312722&centery=6349087.28387397&zoom=5&layer=map&ls=o"
+url = "https://kso.etjanster.lantmateriet.se/?e=535815&n=6459250&z=9&profile=default_background_noauth"
 fileName = "out.png"
 
-browser = webdriver.PhantomJS()
+#browser = webdriver.PhantomJS()
+# PhantomJS does not work with Lantmateriet
+# Therefore, start Firefox headlessly:
+# 1. Install xvfb
+# 2. Start it at display 10: `Xvfb :10 -ac &`
+# 3. Set display to 10: `export DISPLAY=:10`
+# 4. Now run this script from teh same terminal
+#
+# Source: http://agiletesting.blogspot.se/2016/01/running-selenium-webdriver-tests-using.html
+
+browser = webdriver.Firefox()
 browser.set_window_size(2000,2000)
 browser.get(url)
 sleep(10)
