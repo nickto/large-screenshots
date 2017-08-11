8b7167e8ec2f2a8341a622c2042b4c3f7431a396
Nikolajs <nickto0x64@gmail.com>
2017-08-11 10:23:45 +0300

    Remove duplicate comments

--- map-extractor.py
+++ map-extractor.py
@@ -11,16 +11,6 @@ if not os.path.exists(dirName):
 url = "https://kso.etjanster.lantmateriet.se/?e=535815&n=6459250&z=9&profile=default_background_noauth"
 fileName = "out.png"
 
-# browser = webdriver.PhantomJS()
-# PhantomJS does not work with Lantmateriet
-# Therefore, start Firefox headlessly:
-# 1. Install xvfb
-# 2. Start it at display 10: `Xvfb :10 -ac &`
-# 3. Set display to 10: `export DISPLAY=:10`
-# 4. Now run this script from teh same terminal
-#
-# Source: http://agiletesting.blogspot.se/2016/01/running-selenium-webdriver-tests-using.html
-
 browser = webdriver.Firefox()
 browser.set_window_size(2000,2000)
 browser.get(url)
